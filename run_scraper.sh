e106f8f35484a35b95c2693b3327b2f9b202317e
Eagle19243 <eagle19243@gmail.com>
2020-02-04 10:53:32 +0200

    add script to run scraper

---
+++ run_scraper.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Initialize options for gunicorn
+OPTS=(
+  --env FLASK_APP=florida_sos
+  --env FLASK_ENV=development
+  --env FLORIDA_SOS_CONF=conf.yml
+  --access-logfile -
+  --error-logfile -
+  --log-level debug
+  --timeout 10800
+  -b 0.0.0.0:8093
+  --reload
+)
+
+#Run gunicorn
+gunicorn "${OPTS[@]}" florida_sos.application:APP
